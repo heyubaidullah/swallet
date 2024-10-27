99131c68d835fe56b83a3aa035bb84a8236f9912
Christina <christinarduthie@gmail.com>
2024-10-27 04:17:31 -0500

    Display balance and chart using ChartJs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+
 
 const Home = () => {
+  const loggedIn = {firstName:'Chris'};
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+        <HeaderBox 
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstName || 'Guest'}
+        subtext="Simplify Access and Control Over Your Account and Transactions"
+        />
+        <TotalBalanceBox 
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={1250.35}
+        />
+        </header>
+      </div>
+    </section>
   )
 }
 
