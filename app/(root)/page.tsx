7f8b5a1acca1d32eaf77e24b931256f83aabaf9c
Christina <christinarduthie@gmail.com>
2024-10-27 05:38:07 -0500

    side bars

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import HeaderBox from '@/components/HeaderBox'
 import TotalBalanceBox from '@/components/TotalBalanceBox';
-
+import RightSidebar from '@/components/RightSidebar';
 
 const Home = () => {
   const loggedIn = {firstName:'Chris'};
@@ -20,7 +20,14 @@ const Home = () => {
         totalCurrentBalance={1250.35}
         />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar 
+      user={loggedIn}
+      transactions={[]}
+      banks={[]}
+      />
+
     </section>
   )
 }
